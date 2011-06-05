53a7a142178bf12941f253bc06eef2a351ebe819
Bonfa <francesco.bonfadelli@gmail.com>
2011-06-05 22:26:03 +0200

    rotazione effettuata correttamente

--- PLATE_DETECTOR/main.cpp
+++ PLATE_DETECTOR/main.cpp
@@ -10,18 +10,19 @@
 PERFEZIONAMENTI
 
 calcolare i valori delle threshold
-
-
 */
+
 double pendenzaRetta(CvPoint * p1, CvPoint * p2);
-int MaxHorzStraightLineDetection(IplImage * src, CvPoint * p1, CvPoint * p2);
+double MaxHorzStraightLineDetection(IplImage * src);
 void elaboraImmagine(IplImage * img);
 double angle( CvPoint* pt1, CvPoint* pt2, CvPoint* pt0 );
 void threshold(IplImage * img, CvPoint * p);
 void setDimension(IplImage * img, int * height, int * width);
-void bonfa();
-
-
+//void bonfa();
+void myHoughLines(IplImage * img, double *H, double *R, int *count, int *maxPend);
+void disegnaRetteHR(IplImage * img,double *H, double *R, int count);
+void disegnaRetteMQ(IplImage * img,double *M, double *Q, char * verticale, int count);
+void antitrasformata(double *H, double *R, double *M, double *Q, char * verticale,int count, int rMax);
 
 void applyThreshold(IplImage * img , double th){
 	int i,j;
@@ -173,15 +174,15 @@ int main(){
 	char* immagineIntermedia = "saved.tif";
 
 
-	//img= cvLoadImage("test/targa_grande2.jpg",0);
+	//img= cvLoadImage("test/targa_grande1.jpg",0);
 	//img= cvLoadImage("test/targa_media2.jpg",0);
 	img= cvLoadImage("test/targa_piccola3.jpg",0);
 	assert(img->depth== IPL_DEPTH_8U);
 	findPlate(img);
 
-	
+	cvShowImage("pre-processing",img);
 	elaboraImmagine(img);
-	cvShowImage("originale",img);
+	cvShowImage("post-processing",img);
 	cvSaveImage(immagineIntermedia,img,0);
 	
 	printf("Immagine salvata\n");
@@ -195,67 +196,6 @@ int main(){
 
 }
 
-/** Cerca la retta orizzontale più lunga.
-	IplImage * src - l'immagine in cui cercare la retta
-	cvPoint * p1 - Il primo punto che limita la retta
-	cvPoint * p2 - Il secondo punto che limita la retta
-*/
-int MaxHorzStraightLineDetection(IplImage * src, CvPoint * p1, CvPoint * p2){
-	CvSeq* lines;
-	double maxLength=0;
-	int countMaxLength=-1;
-
-	IplImage *edge = cvCreateImage(cvGetSize(src),src->depth,src->nChannels);
-	IplImage * horzEdge = cvCreateImage(cvGetSize(src), IPL_DEPTH_32F, 1);
-	CvMemStorage * storage = cvCreateMemStorage(0);
-	
-	cvCanny(src,edge,250,250,3);
-	cvShowImage("edge",edge);
-	
-	IplImage * edgeU = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
-	//cvConvertScale(horzEdge,edgeU,edge->depth/IPL_DEPTH_8U,0);
-	//cvCopy(edge,edgeU);
-	//cvShowImage("horzU",edgeU);
-
-	//cvConvertScale(edge,horzEdge,1,0);
-	//cvCopy(edge,horzEdge);
-
-	cvSobel(edge, horzEdge, 0, 1, 3);
-	cvShowImage("horzEdge",horzEdge);
-		
-	//IplImage * edgeU = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
-	cvConvertScale(horzEdge,edgeU,IPL_DEPTH_32F/IPL_DEPTH_8U,0);
-	cvShowImage("horzU",edgeU);
-	
-
-	IplImage * righe = cvCreateImage(cvGetSize(src), IPL_DEPTH_32F, 4);
-	lines = cvHoughLines2(edgeU, storage, CV_HOUGH_PROBABILISTIC , 8, CV_PI/1800, 10, 11,4);
-	//lines = cvHoughLines2(edgeU, storage, CV_HOUGH_PROBABILISTIC , 0.1, CV_PI/1800, 10, 11,4);
-	//lines = cvHoughLines2(edgeU, storage, CV_HOUGH_STANDARD , 0.01, 0.01, 1.5, 0,0);
-	for(int i=0;i<lines->total;i++){
-		CvPoint* line = (CvPoint*)cvGetSeqElem(lines,i);
-		double length = sqrt(pow(double(line[0].y-line[1].y),2)+pow(double(line[0].x-line[1].x),2));
-		if(length>maxLength){
-			maxLength = length;
-			countMaxLength = i;
-		}
-		cvLine(righe,line[0],line[1],cvScalar(255,255,255,255),1,8,0);
-	}
-	if (countMaxLength != -1){
-		CvPoint* line = (CvPoint*)cvGetSeqElem(lines,countMaxLength);
-		p1->x = line[0].x;
-		p1->y = line[0].y;
-		p2->x = line[1].x;
-		p2->y = line[1].y;
-		cvLine(righe,line[0],line[1],cvScalar(255,0,0,0),3,8,0);
-		cvShowImage("righe",righe);
-		//cvWaitKey(0);
-		return 1;
-	}
-	cvShowImage("righe",righe);
-	//cvWaitKey(0);
-	return 0;
-}
 
 /**Effettua l'elaborazione che precede l'ocr*/
 void elaboraImmagine(IplImage * img){
@@ -264,17 +204,15 @@ void elaboraImmagine(IplImage * img){
 	CvPoint * minPoint, * maxPoint;
 	minPoint = &cvPoint(-1,-1);
 	maxPoint = &cvPoint(-1,-1);
-	if (MaxHorzStraightLineDetection(img,minPoint,maxPoint)){
-		double angoloRotazione = pendenzaRetta(minPoint,maxPoint);
-		if(angoloRotazione != 0){
+	double angoloRotazione=0;
+	if ((angoloRotazione=MaxHorzStraightLineDetection(img))!=0){
 			CvMat *mapMatrix = cvCreateMat(2,3, CV_32F);
 			//cv2DRotationMatrix(cvPoint2D32f(((double) img->height)/2.0,((double) img->width)/2.0),angoloRotazione*180/CV_PI,1.0,mapMatrix);
-			cv2DRotationMatrix(cvPoint2D32f(((double) width)/2.0,((double) height)/2.0),angoloRotazione*180/CV_PI,1.0,mapMatrix);
+			cv2DRotationMatrix(cvPoint2D32f(((double) width)/2.0,((double) height)/2.0),angoloRotazione*180.0/CV_PI,1.0,mapMatrix);
 			IplImage * rotated = cvCreateImage(cvGetSize(img),img->depth,img->nChannels);
 			cvWarpAffine(img, rotated, mapMatrix);
 			cvCopy(rotated,img,0);
 			//cvShowImage("ruotata",img);
-		}
 	}
 
 	//cvLine(img,*minPoint,*maxPoint,cvScalar(0,0,0,0),1,8,0);
@@ -300,22 +238,22 @@ void threshold(IplImage * img, CvPoint * p){
 	}
 }
 
-void bonfa(){
-	char* nomeImmagine = "leon.jpg";
-	char* immagineIntermedia = "saved.tif";
-
-	IplImage *img = cvLoadImage(nomeImmagine,0);
-	elaboraImmagine(img);
-	cvShowImage("originale",img);
-	cvSaveImage(immagineIntermedia,img,0);
-	
-	printf("Immagine salvata\n");
-	printf("Passo l'immagine a Tesseract per l'ocr\n");
-	//ocr(immagineIntermedia);  //decommentare una volta installato tesseract
-
-	//cvWaitKey(0);
-	exit(0);
-}
+//void bonfa(){
+//	char* nomeImmagine = "leon.jpg";
+//	char* immagineIntermedia = "saved.tif";
+//
+//	IplImage *img = cvLoadImage(nomeImmagine,0);
+//	elaboraImmagine(img);
+//	cvShowImage("originale",img);
+//	cvSaveImage(immagineIntermedia,img,0);
+//	
+//	printf("Immagine salvata\n");
+//	printf("Passo l'immagine a Tesseract per l'ocr\n");
+//	//ocr(immagineIntermedia);  //decommentare una volta installato tesseract
+//
+//	//cvWaitKey(0);
+//	exit(0);
+//}
 
 /** Assegna a width e height passati come parametri i corrispettivi valori della ROI dell'immagine*/
 void setDimension(IplImage * img, int *height, int *width){
@@ -327,4 +265,180 @@ void setDimension(IplImage * img, int *height, int *width){
 		*height = img->roi->height;
 		*width = img->roi->width;
 		}
+}
+
+/** Cerca la retta orizzontale più lunga.
+	IplImage * src - l'immagine in cui cercare la retta
+	cvPoint * p1 - Il primo punto che limita la retta
+	cvPoint * p2 - Il secondo punto che limita la retta
+*/
+double MaxHorzStraightLineDetection(IplImage * src){
+	double maxLength=0;
+	int countMaxLength=-1;
+
+	IplImage *edge = cvCreateImage(cvGetSize(src),src->depth,src->nChannels);
+	IplImage * horzEdge = cvCreateImage(cvGetSize(src), IPL_DEPTH_32F, 1);
+	//CvMemStorage * storage = cvCreateMemStorage(0);
+	
+	cvCanny(src,edge,50,150,3);
+	cvShowImage("edge",edge);
+	
+	IplImage * edgeU = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+	cvConvertScale(horzEdge,edgeU,edge->depth/IPL_DEPTH_8U,0);
+	
+	cvSobel(src, horzEdge, 0, 1, 3);
+	cvShowImage("horzEdge",horzEdge);
+		
+	cvConvertScale(horzEdge,edgeU,IPL_DEPTH_32F/IPL_DEPTH_8U,0);
+	cvShowImage("horzU",edgeU);
+	
+
+	
+	
+	double * h = (double *)malloc(100*sizeof(double));
+	double * r = (double *)malloc(100*sizeof(double));
+	int count = 0;
+	int maxPend = -1;
+	myHoughLines(edgeU,h,r,&count,&maxPend);
+
+	/*for(int i=0;i<count;i++){
+		printf("%2.2f\t %2.2f\n",m[i],q[i]);
+	}*/
+
+	
+	IplImage * righe = cvCreateImage(cvGetSize(src), IPL_DEPTH_32F, 1);
+	disegnaRetteHR(righe,h,r,count);
+
+	//m e q sono due vettori che contengono H e R
+	//delle count rette con lunghezza maggiore dell'immagine
+	double * m = (double *)malloc(100*sizeof(double));
+	double * q = (double *)malloc(100*sizeof(double));
+	char * verticale = (char *)malloc(100*sizeof(char));
+	int rMax = (int) floor((double)sqrt(pow((double)edgeU->height,2)+pow((double)edgeU->width,2)))+1;
+	antitrasformata(h,r,m,q,verticale,count,rMax);
+	IplImage * righe2 = cvCreateImage(cvGetSize(src), IPL_DEPTH_32F, 1);
+	disegnaRetteMQ(righe2,m,q,verticale,count);
+
+	//int p = 1;
+	//double h_rad = h[p]*CV_PI/180;
+	//for(int j=0;j<src->width;j++){
+	//	double  i = (r[p]-rMax-j*cos(h_rad))/sin(h_rad);
+	//	//if(i>=0 && i<src->height){
+	//			printf("H=%2.4f\tR=%2.4f\tI=%2.2f\tJ=%d\n",h_rad,r[p],i,j);
+	//			printf("M=%2.4f\tQ=%2.4f\tI=%2.2f\tJ=%d\n\n",m[p],q[p],m[p]*j+q[p],j);
+	//}
+	
+	free(h);
+	free(r);
+	double angolo = 0;
+	if (maxPend!=-1)
+		angolo = m[maxPend];
+	else if (count != 0)
+		angolo = m[0];
+	free(m);
+	free(q);
+	free(verticale);
+	return angolo;
+}
+
+void myHoughLines(IplImage * img, double *H, double *R, int *count, int *maxPend){
+	IplImage *edge,*accumulation;
+	
+	//Edge Detection
+	edge = cvCreateImage(cvGetSize(img),img->depth,1);
+	cvCanny(img,edge,10,100,3);
+
+	//Creazione della ACCUMULATION MATRIX
+	int thetaMax = 180;
+	int rMax = (int) floor((double)sqrt(pow((double)img->height,2)+pow((double)img->width,2)))+1;
+	accumulation = cvCreateImage(cvSize(2*rMax,thetaMax),img->depth,1);
+	for (int i=0;i<accumulation->height;i++)
+		for(int j=0;j<accumulation->width;j++)
+			cvSet2D(accumulation,i,j,cvRealScalar(0));
+
+	//Disegno la sinusoide per ogni punto dell'edge
+	for(int i=0;i<img->height;i++)
+		for(int j=0;j<img->width;j++)
+			if (cvGet2D(edge,i,j).val[0]==255)
+				for(int h=0;h<thetaMax;h++){ // h cicla su theta
+					double h_rad = h*CV_PI/180;
+					double r = j*cos(h_rad)+i*sin(h_rad)+ rMax; //calcolo di r
+					//printf("H=%d\tR=%2.2f\n",h,r);
+					cvSet2D(accumulation,h,(int)r,cvRealScalar(cvGet2D(accumulation,h,(int)r).val[0]+1));
+					}
+
+	//Seleziono i punti di massimo della matrice di accumulazione e memorizzo h e r in una sequenza
+	double soglia;
+	cvMinMaxLoc(accumulation,0,&soglia,0,0,0);
+	soglia = soglia - 10;
+	int quante = 0;
+	*maxPend = -1;
+	for(int h=0;h<accumulation->height;h++)
+		for(int l=0;l<accumulation->width;l++)
+			if (cvGet2D(accumulation,h,l).val[0] > soglia){
+				if(quante < 100) {
+					//double h_rad = ((double)h)*CV_PI/180.0;
+					H[quante] = h;
+					R[quante] = l;
+					quante++;
+					if (cvGet2D(accumulation,h,l).val[0]==soglia+10 && *maxPend==-1)
+						*maxPend = quante-1;
+				}
+			}
+	*count = quante;
+}
+
+
+/** Disegna le count rette descritte dalle coppie H e R, parametri della trasformata di Hough */
+void disegnaRetteHR(IplImage * img,double *H, double *R, int count){
+	int rMax = (int) floor((double)sqrt(pow((double)img->height,2)+pow((double)img->width,2)))+1;
+	for(int p=0;p<count;p++) {
+			double h_rad = H[p]*CV_PI/180.0;
+			if (H[p]!=0)
+			for(int j=0;j<img->width;j++){
+				//Disegna la retta
+				double  i = (R[p]-rMax-j*cos(h_rad))/sin(h_rad);
+				if(i>=0 && i<img->height)
+					//printf("H=%d\tL=%d\tI=%2.2f\tJ=%d\n",h,l,i,j);
+					cvSet2D(img,(int)i,j,cvRealScalar(255));
+				}
+			else
+				for(int i=0;i<img->height;i++)
+					cvSet2D(img,i,R[p]-rMax,cvRealScalar(255));	
+		}
+	cvShowImage("rette",img);
+}
+
+/** Calcola M e Q delle count rette descritte dalle coppie H e R della trasformata di Hough 
+  * con origine il centro dell'immagine */
+void antitrasformata(double *H, double *R, double *M, double *Q, char * verticale,int count,int rMax) {
+	for(int p=0;p<count;p++) {
+			double h_rad = H[p]*CV_PI/180.0;
+			if (H[p]!=0){
+				M[p] = -cos(h_rad)/sin(h_rad);
+				Q[p] = ((double)(R[p]-rMax))/sin(h_rad);
+				verticale[p] = 0;
+			}
+			else {
+				M[p]=0;
+				Q[p]=R[p]-rMax;
+				verticale[p]=1;
+			}
+	}
+}
+
+/** Disegna le count rette descritte dalle coppie M e Q e dal flag verticale */
+void disegnaRetteMQ(IplImage * img,double *M, double *Q, char * verticale, int count){
+	double i;
+	for(int p=0;p<count;p++) {
+			if (!verticale[p])
+				for(int j=0;j<img->width;j++) {
+					//printf("M=%2.4f\tQ=%2.4f\tI=%2.2f\tJ=%d\n",M[p],Q[p],M[p]*j+Q[p],j);
+					if((i=M[p]*j+Q[p])>=0 && i<img->height)
+						cvSet2D(img,(int)i,j,cvRealScalar(255));}
+			else
+				for(int i=0;i<img->height;i++)
+					cvSet2D(img,i,Q[p],cvRealScalar(255));	
+		}	
+	cvShowImage("retteMQ",img);
 }
